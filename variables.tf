4b244dbce4e2d0e2c4574a99148d9e2248f279bd
nishantabanik <36991851+nishantabanik@users.noreply.github.com>
2023-01-03 10:00:57 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-/**
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 variable "project_id" {
   type = string
@@ -38,3 +23,10 @@ variable "lb_name" {
   description = "Name for load balancer and associated resources"
   default     = "tf-cr-lb"
 }
+
+variable "service_account_email" {
+  description = "Service account email to run containers under"
+  type        = string
+  default = "servacc001@playground-s-11-503185d7.iam.gserviceaccount.com "
+}
+
