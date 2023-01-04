700f031d58dd31e6da3f7c7c79ff52ed8f5c3f4c
nishantabanik <nishanta.banik2016@gmail.com>
2023-01-04 16:57:27 +0100

    3rd change

--- iac-terraform/cloudbuild/main.tf
+++ iac-terraform/cloudbuild/main.tf
@@ -1,22 +1,3 @@
-/* variable "gcp_service_list" {
-  description ="The list of apis necessary for the project"
-  type = list(string)
-  default = [
-    "cloudresourcemanager.googleapis.com",
-    "serviceusage.googleapis.com",
-    "dns.googleapis.com",
-    "iam.googleapis.com",
-    "run.googleapis.com",
-    "cloudbuild.googleapis.com"
-  ]
-}
-
-resource "google_project_service" "gcp_services" {
-  for_each = toset(var.gcp_service_list)
-  project = "playground-s-11-572e388d"
-  service = each.key
-} */
-
 
 resource "google_project_service" "cloudbuild_api" {
   project  = var.project_id
@@ -34,26 +15,42 @@ resource "google_cloudbuild_trigger" "build-prod" {
   disabled    = false # Status is enabled by default
   depends_on = [google_project_service.cloudbuild_api]
 
+  # Connect to GitHub and trigger on push to develop branch
+
+  trigger_template {
+    repo_name   = "https://github.com/nishantabanik/cloud-build-samples.git"
+    branch_name = "main"
+    dir         = "basic-config"
+  }
+
+  service_account = var.service_account_email
+}
+
 /*   approval_config {
     approval_required = yes
   } */
 
-  # Connect to GitHub and trigger on push to develop branch
-  # with specific glob pattern
-  trigger_template {
-    repo_name   = "https://github.com/nishantabanik/cloud-build-samples.git"
-    branch_name = "main"
-    #tag_name = ""
-    dir         = "basic-config"
-    #pattern     = "jest.config.js"
-  }
-
-  # Use dedicated service account to build the application
-  service_account = var.service_account_email
-
-  # Optionally, toggle on/off manual approval
+    # Optionally, toggle on/off manual approval
   /* options {
     substitution_option = "ALLOW_LOOSE"
     disable_manual_triggers = false
   } */
+
+  /* variable "gcp_service_list" {
+  description ="The list of apis necessary for the project"
+  type = list(string)
+  default = [
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "dns.googleapis.com",
+    "iam.googleapis.com",
+    "run.googleapis.com",
+    "cloudbuild.googleapis.com"
+  ]
 }
+
+resource "google_project_service" "gcp_services" {
+  for_each = toset(var.gcp_service_list)
+  project = "playground-s-11-572e388d"
+  service = each.key
+} */
