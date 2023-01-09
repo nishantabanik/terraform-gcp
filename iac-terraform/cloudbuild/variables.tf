5a48a5aa399a28782343f3180dc09c9ee13e1e07
nishantabanik <36991851+nishantabanik@users.noreply.github.com>
2023-01-09 11:51:06 +0100

    Update variables.tf

--- iac-terraform/cloudbuild/variables.tf
+++ iac-terraform/cloudbuild/variables.tf
@@ -1,7 +1,12 @@
 
 variable "project_id" {
-  type = string
+  type    = string
   default = "lgf-devops-sandbox"
+}
+
+variable "repo_name" {
+  type    = string
+  default = "https://github.com/nishantabanik/cloud-build-samples.git"
 }
 
 variable "region" {
@@ -12,24 +17,5 @@ variable "region" {
 variable "service_account_email" {
   description = "Service account email to run containers under"
   type        = string
-  default = "abcmnop001"
+  default     = "abcmnop001"
 }
-
-/* variable "ssl" {
-  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
-  type        = bool
-  default     = true
-}
-
-variable "domain" {
-  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
-  type        = string
-}
-
-variable "lb_name" {
-  description = "Name for load balancer and associated resources"
-  default     = "tf-cr-lb"
-} */
-
-
-
