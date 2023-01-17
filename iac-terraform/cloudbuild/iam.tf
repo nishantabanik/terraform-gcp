842e3d0457994d7251f4c91fab0df80f73d35b23
nishantabanik <36991851+nishantabanik@users.noreply.github.com>
2023-01-17 15:50:56 +0100

    Update iam.tf

--- iac-terraform/cloudbuild/iam.tf
+++ iac-terraform/cloudbuild/iam.tf
@@ -1,18 +1,3 @@
-data "google_project" "project" {}
-
-resource "google_cloudbuild_trigger" "service-account-trigger" {
-  trigger_template {
-    branch_name = "main"
-    repo_name   = var.repo_name
-  }
-
-  service_account = google_service_account.cloudbuild_service_account.id
-  filename        = "cloudbuild.yaml"
-  depends_on = [
-    google_project_iam_member.act_as,
-    google_project_iam_member.logs_writer
-  ]
-}
 
 resource "google_service_account" "cloudbuild_service_account" {
   account_id = var.service_account_email
@@ -20,14 +5,23 @@ resource "google_service_account" "cloudbuild_service_account" {
 
 resource "google_project_iam_member" "act_as" {
   project = data.google_project.project.project_id
-  #role    = "roles/cloudbuild.builds.editor"
-  role    = "roles/iam.serviceAccountUser"
+  role    = "roles/cloudbuild.builds.editor"
+  #role    = "roles/iam.serviceAccountAdmin"
   member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
 
 resource "google_project_iam_member" "logs_writer" {
   project = data.google_project.project.project_id
-  role    = "roles/logging.logWriter"
-  #role    = "roles/editor"
+  #role    = "roles/logging.logWriter"
+  role    = "roles/editor"
   member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+resource "google_service_account_iam_binding" "binding" {
+  service_account_id = google_service_account.cloudbuild_service_account.name
+  role = "roles/editor"
+  members = [
+    "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+    ]
+  depends_on = [google_service_account.cloudbuild_service_account]
 }
