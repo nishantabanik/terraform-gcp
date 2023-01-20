ab1168d7433bd3bcf21fda64cb6996c2c5bf823d
nishantabanik <36991851+nishantabanik@users.noreply.github.com>
2023-01-20 12:48:58 +0100

    Update variables.tf

--- iac-terraform/cloudrun/variables.tf
+++ iac-terraform/cloudrun/variables.tf
@@ -15,21 +15,4 @@ variable "service_account_email" {
   default = "abcmnop001"
 }
 
-/* variable "ssl" {
-  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
-  type        = bool
-  default     = true
-}
-
-variable "domain" {
-  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
-  type        = string
-}
-
-variable "lb_name" {
-  description = "Name for load balancer and associated resources"
-  default     = "tf-cr-lb"
-} */
-
-
 
