95996cc249af685843f3bffe9cd5cc0ad14e31ec
nishantabanik <36991851+nishantabanik@users.noreply.github.com>
2023-01-23 11:37:45 +0100

    Update iam.tf

--- iac-terraform/cloudbuild/iam.tf
+++ iac-terraform/cloudbuild/iam.tf
@@ -6,22 +6,20 @@ resource "google_service_account" "cloudbuild_service_account" {
 resource "google_project_iam_member" "act_as" {
   project = data.google_project.project.project_id
   role    = "roles/cloudbuild.builds.editor"
-  #role    = "roles/iam.serviceAccountAdmin"
-  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+  member = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
 
 resource "google_project_iam_member" "logs_writer" {
   project = data.google_project.project.project_id
-  #role    = "roles/logging.logWriter"
-  role    = "roles/editor"
-  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+  role    = "roles/logging.logWriter"
+  member = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
 
 resource "google_service_account_iam_binding" "binding" {
   service_account_id = google_service_account.cloudbuild_service_account.name
-  role = "roles/editor"
+  role               = "roles/iam.serviceAccountAdmin"
   members = [
     "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
-    ]
+  ]
   depends_on = [google_service_account.cloudbuild_service_account]
 }
