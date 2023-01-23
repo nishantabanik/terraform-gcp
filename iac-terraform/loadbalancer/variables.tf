081d1cfd0803d7169e02713fbf72ec5d5c1d9333
nishantabanik <36991851+nishantabanik@users.noreply.github.com>
2023-01-23 18:47:38 +0100

    Update variables.tf

--- iac-terraform/loadbalancer/variables.tf
+++ iac-terraform/loadbalancer/variables.tf
@@ -1,12 +1,18 @@
 
 variable "project_id" {
-  type = string
+  type    = string
   default = "lgf-devops-sandbox"
 }
 
 variable "region" {
   description = "Location for load balancer and Cloud Run resources"
   default     = "europe-west1"
+}
+
+variable "cloud_run_name" {
+  description = "Cloud run instance for BILD application"
+  type        = string
+  default     = "dev-de-run-bild"
 }
 
 variable "ssl" {
@@ -22,12 +28,21 @@ variable "domain" {
 
 variable "lb_name" {
   description = "Name for load balancer and associated resources"
-  default     = "tf-cr-lb"
+  default     = "tf-dev-bild-lb"
+}
+
+variable "ip_address" {
+  default     = ("10.0.10.3")
 }
 
 variable "service_account_email" {
   description = "Service account email to run containers under"
   type        = string
-  default = "servacc001"
+  default     = "dev-load-balancer-sa"
 }
 
+variable "serverless_neg_name" {
+  description = "Serverless NEG for BILD loadbalancer"
+  type        = string
+  default     = "dev-de-bild-lb-neg"
+}
